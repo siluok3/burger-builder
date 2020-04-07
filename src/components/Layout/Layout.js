f4a67e0b9c82f0efd803fe558e5991eb2a240c35
Kiriakos Papachristou <koulispapas@gmail.com>
2020-04-07 16:56:33 +0200

    create leayout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const layout = props => {
+  return (
+    <React.Fragment>
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main>
+        {props.children}
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default layout;
