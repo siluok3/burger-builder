499c4d2ff93d4532d5005c057c990e3ea60e8f3f
Kiriakos Papachristou <koulispapas@gmail.com>
2020-05-07 13:06:45 +0200

    Add form validation for inputs

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,7 +17,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -25,7 +30,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -33,7 +43,14 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'ZIP'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -41,7 +58,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -49,7 +71,11 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -59,9 +85,12 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'}
           ]
         },
-        value: 'fastest'
+        value: 'fastest',
+        validation: {},
+        valid: true
       }
     },
+    isFormValid: false,
     loading: false
   };
 
@@ -90,13 +119,41 @@ class ContactData extends Component {
       });
   }
 
+  checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  };
+
   inputHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {...this.state.orderForm};
     const updatedFormElement = {...updatedOrderForm[inputIdentifier]};
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    
+    let isFormValid = true;
+    for (let input in updatedOrderForm) {
+      isFormValid = updatedOrderForm[input].valid && isFormValid;
+    }
 
-    this.setState({ orderForm: updatedOrderForm });
+    this.setState({
+      orderForm: updatedOrderForm,
+      isFormValid: isFormValid
+    });
   }
 
   render() {
@@ -116,9 +173,12 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(e) => this.inputHandler(e,formElement.id)} />
         ))}
-        <Button btnType="Success">Send your order</Button>
+        <Button btnType="Success" disabled={!this.state.isFormValid}>Send your order</Button>
       </form>
     );
     if (this.state.loading) {
