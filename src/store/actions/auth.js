b8778c07e83fd53a82e9447958078bd87b6438ba
Kiriakos Papachristou <koulispapas@gmail.com>
2020-06-11 11:38:01 +0200

    Add authentication and handle it on the redux store

---
+++ src/store/actions/auth.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionsTypes';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  }
+}
+
+export const authSuccess = (token, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: token,
+    userId: userId
+  }
+}
+
+export const authFail = (error) => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  }
+}
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  }
+}
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAFAxwU44ZkpSIjC9wVN-TlDnCvVlm8nVQ';
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAFAxwU44ZkpSIjC9wVN-TlDnCvVlm8nVQ';
+    }
+    axios.post(url, authData)
+      .then(response => {
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn))
+      })
+      .catch(err => dispatch(authFail(err.response.data.error)));
+  }
+}
